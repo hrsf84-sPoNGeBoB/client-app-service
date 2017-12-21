f0a305f723787b8cd6481694d8216396d8c9f27b
Alan Chin <chinalanq@gmail.com>
2017-12-21 11:16:04 -0800

    Finish videos importer script; add functionality for models

--- models/videos.js
+++ models/videos.js
@@ -1,16 +1,52 @@
 const client = require('../db').esearchClient;
 
-const addVideoEntry = (video) => {
-  client.create({
-    index: 'videos',
-    type: 'video',
-    body: video,
+const INDEX = 'testing';
+const TYPE = 'videos';
+
+const addVideoEntry = video => (
+  new Promise((resolve, reject) => {
+    client.create({
+      index: INDEX,
+      type: TYPE,
+      body: video,
+    }, (error, response) => {
+      if (error) reject(error);
+      else resolve(response);
+    });
+  })
+);
+
+const batchInsertVideos = (videos) => {
+  const body = [];
+  const action = {
+    index: {
+      _index: INDEX,
+      _type: TYPE,
+    },
+  };
+
+  // add videos to body
+  videos.forEach(video => body.push(action, video));
+
+  return new Promise((resolve, reject) => {
+    client.bulk({ body }, (error, response) => {
+      if (error) reject(error);
+      else resolve(response);
+    });
   });
 };
 
-const queryResults = q => client.search({ q });
+const queryResults = q => (
+  new Promise((resolve, reject) => {
+    client.search({ q, index: INDEX }, (error, response) => {
+      if (error) reject(error);
+      else resolve(response);
+    });
+  })
+);
 
 module.exports = {
   addVideoEntry,
+  batchInsertVideos,
   queryResults,
 };
--- scripts/data-importer.js
+++ scripts/data-importer.js
@@ -7,34 +7,51 @@ const videosClient = require('../models/videos');
 const channelsFile = './data/mock-channel-data.json';
 const videosFile = './data/mock-video-data.json';
 
-const BATCH_LEN = 10;
+const CHANNEL_BATCH_LEN = 10;
+const VIDEO_BATCH_LEN = 25;
 
 (function () {
   const readChannels = readline.createInterface({ input: fs.createReadStream(channelsFile) });
-  let channels = [];
+  let batch = [];
 
   readChannels.on('line', (line) => {
-    channels.push(JSON.parse(line));
+    batch.push(JSON.parse(line));
 
-    if (channels.length >= BATCH_LEN) {
+    if (batch.length >= CHANNEL_BATCH_LEN) {
       try {
-        // const batch = async () => { await channelsClient.batchInsertChannels(channels); };
-        // batch();
-        channels = [];
+        // const fn = async () => { await channelsClient.batchInsertChannels(batch); };
+        // fn();
       } catch (e) {
         console.log('Bad batch insert', e);
       }
+      batch = [];
     }
   });
 
   readChannels.on('close', () => {
-    channels = undefined;
-    console.log('Done inserting channels.');
+    console.log('Done inserting channels');
 
     const readVideos = readline.createInterface({ input: fs.createReadStream(videosFile) });
+    batch = [];
+    let count = 0;
+    const startTime = moment();
 
     readVideos.on('line', (line) => {
-      console.log(line);
+      count += 1;
+      if (count % 100000 === 0) console.log(count, moment().diff(startTime));
+      batch.push(JSON.parse(line));
+
+      if (batch.length >= VIDEO_BATCH_LEN) {
+        try {
+          const fn = async () => { await videosClient.batchInsertVideos(batch); };
+          fn();
+        } catch (e) {
+          console.log('Bad batch insert');
+        }
+        batch = [];
+      }
     });
+
+    readVideos.on('close', () => console.log('Done inserting videos'));
   });
 }());
